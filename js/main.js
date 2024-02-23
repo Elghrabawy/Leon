9fe44c6b305d8499c2586fedfba9cac8be4abeca
Ebrahim Elghrbawy <HIMA132004@GMAIL.COM>
2024-02-23 21:39:15 +0200

    icon bar onclick even

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function open_bar() {
+  let bar = document.getElementById("bar_links");
+  if (bar.style.display == "block") bar.style.display = "none";
+  else bar.style.display = "block";
+}
+
+function hide_links_bar() {
+  let bar = document.getElementById("bar_links");
+  if (bar.style.display == "block") bar.style.display = "none";
+}
